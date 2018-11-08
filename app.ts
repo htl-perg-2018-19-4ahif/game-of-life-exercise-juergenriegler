462a1f5f764610f3107bf9ec1be7b65c2d8007aa
juergenriegler <juergen.riegler@gmx.net>
2018-11-08 12:39:44 +0100

    game finished

--- app.ts
+++ app.ts
@@ -1,20 +1,66 @@
 window.onload = () => {
-  const boardSize = 800;
 
-  // Get reference to canvas
+  const boardSize = 200;
   const canvas = <HTMLCanvasElement>document.getElementById('canvas');
-  canvas.width = canvas.height = boardSize;
+  canvas.width = canvas.height = boardSize*2;
   const ctx = canvas.getContext('2d');
   ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+  
+  let matrix = [];
+  for(let i=0; i<boardSize+2; i++) matrix.push(getArray());
+  matrix[0] = getEmptyArray();
+  matrix[matrix.length-1] = getEmptyArray();
 
-  // Call 'draw' function whenever browser renders a frame on the screen
+  function getEmptyArray() {
+    let array = [];
+    for(let i=0; i<boardSize+2; i++) array[i] = false;
+    return array;
+  }
+
+  function getArray() {
+    let array = []
+    for(let i=0; i<boardSize+2; i++) array[i] = false; 
+    for(let i=1; i<(boardSize/100)*3; i++) {
+      array[Math.floor(Math.random()*(boardSize-1+1)+1)] = true;
+    }
+    return array;
+  }
+  
+  function update() {
+    for (let i=1; i<matrix.length-1; i++) {
+      for (let j=1; j<matrix[0].length-1; j++) {
+        let counter = 0;
+        if (matrix[i+1][j])counter++;
+        if (matrix[i+1][j-1])counter++;
+        if (matrix[i+1][j+1])counter++;
+        if (matrix[i][j-1])counter++;
+        if (matrix[i][j+1])counter++;
+        if (matrix[i-1][j])counter++;
+        if (matrix[i-1][j-1])counter++;
+        if (matrix[i-1][j+1])counter++;
+        if(matrix[i][j]) {
+          if(counter<2) {
+            matrix[i][j]=false;
+            ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+            ctx.fillRect(i*2,j*2,2,2);
+          }
+          if(counter>3) {
+            matrix[i][j]=false;
+            ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+            ctx.fillRect(i*2,j*2,2,2);
+          }
+        }  
+        else if(counter==3) {
+          matrix[i][j]=true; 
+          ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+          ctx.fillRect(i*2,j*2,2,2); 
+        }        	
+      }
+    }
+  }
   window.requestAnimationFrame(draw);
-
   function draw() {
-    // Demo code showing how to draw in the canvas
-    ctx.clearRect(0, 0, boardSize, boardSize);
-    ctx.fillRect(10, 10, 30, 30);
-
+    update();
     window.requestAnimationFrame(draw);
   }
 };
